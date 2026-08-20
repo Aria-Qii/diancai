b429b2aae0bc66cb3926102f8b4219cb8e6f55af
Aria-Qii <740497484@qq.com>
2026-08-20 09:43:59 +0800

    deploy sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dishwheel-v1';
+const CACHE = 'dishwheel-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
